61701e9527b7135d55e636c4327b9ea5efa5c451
saptarshi <sapbum1234@gmail.com>
2025-01-14 09:37:00 +0530

    Initial commit

---
+++ src/main/java/com/Authentication/smart_auth/SmartAuthApplication.java
@@ -0,0 +1,13 @@
+package com.Authentication.smart_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAuthApplication.class, args);
+	}
+
+}
