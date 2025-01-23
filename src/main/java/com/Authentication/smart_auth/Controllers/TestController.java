633fdf8d98fdbff729b878b1d27fa3366dfde994
saptarshi <sapbum1234@gmail.com>
2025-01-23 16:51:25 +0530

    in memory authentication

--- src/main/java/com/Authentication/smart_auth/Controllers/TestController.java
+++ src/main/java/com/Authentication/smart_auth/Controllers/TestController.java
@@ -1,6 +1,10 @@
 package com.Authentication.smart_auth.Controllers;
 
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +21,20 @@ public class TestController {
         String msg="This is a demo application aimed at learning authentication and AI";
         return msg;
     }
+
+    @GetMapping("/user/hello")
+    public String getUserGreetings(@AuthenticationPrincipal UserDetails user) {
+        return user.getUsername()+" says hello to you!! You are talking to "+user.getAuthorities()+" with password "+user.getPassword();
+    }
+
+//    @GetMapping("/user/update")
+//    public void updateRole(@AuthenticationPrincipal UserDetails user) {
+//        InMemoryUserDetailsManager manager=new InMemoryUserDetailsManager();
+//        user= User.withUsername(user.getUsername())
+//                .password("{noop}newpass")
+//                .roles("USER")
+//                .build();
+//        if(manager.userExists(user.getUsername()))
+//            manager.updateUser(user);
+//    }
 }
--- src/main/java/com/Authentication/smart_auth/Security/BasicSecurityConfig.java
+++ src/main/java/com/Authentication/smart_auth/Security/BasicSecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,7 +26,33 @@ public class BasicSecurityConfig {
                     .anyRequest()).authenticated();
         });
         //http.formLogin(Customizer.withDefaults());
+        // Disables mandatory CSRF token
+        http.csrf(csrf->csrf.disable());
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager=new InMemoryUserDetailsManager();
+        UserDetails user1= User.withUsername("Bumba")
+                .password("{noop}pass1")
+                .roles("USER")
+                .build();
+        UserDetails user2=User.withUsername("samir")
+                .password("{noop}pass2")
+                .roles("USER")
+                .build();
+        UserDetails user3=User.withUsername("juin")
+                .password("{noop}pass3")
+                .roles("ADMIN")
+                .build();
+        if(!manager.userExists(user1.getUsername()))
+            manager.createUser(user1);
+        if(!manager.userExists(user2.getUsername()))
+            manager.createUser(user2);
+        if(!manager.userExists(user3.getUsername()))
+            manager.createUser(user3);
+        return manager;
     }
 }
